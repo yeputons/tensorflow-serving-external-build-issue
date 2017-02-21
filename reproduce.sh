89fddd1a8e2de54d4b042c0baf8d8e242554dfc3
Egor Suvorov <egor.suvorov@samsung.com>
2017-02-21 11:24:55 +0900

    Initial commit

---
+++ reproduce.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -u -e
+
+BAD_TARGET=//tensorflow_serving/apis:prediction_service_proto_py_pb2
+
+echo -e "\e[1;33m========== INITIALIZATION ==========\e[0m"
+git submodule update --init --recursive
+pushd serving/tensorflow
+echo -e '\n\n\n\n\n\n\n\n\n\n' | ./configure
+popd
+
+echo -e "\e[1;33m========== BUILDING FROM THE REPOSITORY ==========\e[0m"
+pushd serving
+bazel build "$BAD_TARGET" --verbose_failures
+popd
+
+echo -e "\e[1;33m========== BUILDING AS EXTERNAL DEPENDENCY ==========\e[0m"
+bazel build "@tf_serving$BAD_TARGET" --verbose_failures
